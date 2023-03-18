033e8a176381137496d7484e320a34a0ad0cbae0
InnovationCell-NITRR <77698217+InnovationCell-NITRR@users.noreply.github.com>
2023-03-19 04:51:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,16 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const serverless = require("serverless-http");
+
+var fs = require('fs');
+var http = require('http');
+var https = require('https');
 var cors = require('cors');
+
+var privateKey  = fs.readFileSync('/etc/letsencrypt/live/13-233-198-227.nip.io/privkey.pem', 'utf8');
+var certificate = fs.readFileSync('/etc/letsencrypt/live/13-233-198-227.nip.io/cert.pem', 'utf8');
+
+var credentials = {key: privateKey, cert: certificate};
 
 const app = express();
 app.use(express.json());
@@ -26,10 +35,16 @@ mongoose
   .then(() => console.log("Mongodb connected successfully"))
   .catch((issues) => console.log("issues " + issues));
 
-if (process.env.ENVIRONMENT === "lambda") {
-  module.exports.handler = serverless(app);
-} else {
-  app.listen(8000, () => {
-    console.log("Server is Running")
-  })
-}
+
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
+
+// if (process.env.ENVIRONMENT === "lambda") {
+//   module.exports.handler = serverless(app);
+// } else {
+//   app.listen(8000, () => {
+//     console.log("Server is Running")
+//   })
+// }
+
+
